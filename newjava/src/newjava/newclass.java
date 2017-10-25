6b3194531faf4b0daa2a83b7e1757574b6bd964b
ROSHAN PINTO <ROSHAN PINTO@ROBINDESK>
2017-10-25 19:48:33 +0530

    commit rich1

--- newjava/src/newjava/newclass.java
+++ newjava/src/newjava/newclass.java
@@ -4,7 +4,7 @@ public class newclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       System.out.println("hi java");
+       System.out.println("hi i am Rich");
 	}
 
 }
